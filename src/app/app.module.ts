018478957b84caa5f417cf6c6f159b18e09dd697
ryma1107 <jebrirym91@gmail.com>
2020-08-22 14:11:45 +0100

    second2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarteVisiteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
